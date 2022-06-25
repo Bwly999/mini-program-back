5127ffff8304b934ea3b0fcc83b251ecfb452cd6
Bwly999 <438225686@qq.com>
2022-06-25 20:54:38 +0800

    update port

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 nohup java -jar ./gateway/target/gateway-1.0.0-SNAPSHOT.jar >> ./logs/gateway.out &
 nohup java -jar ./user/target/user-1.0.0-SNAPSHOT.jar >> ./logs/user.out &
-nohup java -jar ./godds/target/godds-1.0.0-SNAPSHOT.jar >> ./logs/godds.out &
+nohup java -jar ./goods/target/goods-1.0.0-SNAPSHOT.jar >> ./logs/goods.out &
