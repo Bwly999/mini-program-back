c9f40bc682d354318726e77a8084812499f945a4
Bwly999 <438225686@qq.com>
2022-06-26 13:32:00 +0800

    fix duplicate MongoRepositoryConfig Bean

--- goods/src/main/java/cn/edu/xmu/mini/GoodsApplication.java
+++ goods/src/main/java/cn/edu/xmu/mini/GoodsApplication.java
@@ -3,7 +3,7 @@ package cn.edu.xmu.mini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"cn.edu.xmu.mini.config", "cn.edu.xmu.mini.goods", "cn.edu.xmu.mini.orders", "cn.edu.xmu.mini.shop"})
 public class GoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class, args);
