0c1e89f9f84039b18b0226721b96240ec37f371a
Bwly999 <438225686@qq.com>
2022-06-26 21:21:25 +0800

    通过更改maven配置 解决子模块依赖 无法打包的问题

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 nohup java -jar ./gateway/target/gateway-1.0.0-SNAPSHOT.jar >> ./logs/gateway.out &
-nohup java -jar ./user/target/user-1.0.0-SNAPSHOT.jar >> ./logs/user.out &
-nohup java -jar ./goods/target/goods-1.0.0-SNAPSHOT.jar >> ./logs/goods.out &
+nohup java -jar ./user/target/user-1.0.0-SNAPSHOT-exec.jar >> ./logs/user.out &
+nohup java -jar ./goods/target/goods-1.0.0-SNAPSHOT-exec.jar >> ./logs/goods.out &
